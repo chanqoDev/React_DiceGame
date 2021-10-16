02e97a9c4b71e1e01ba0594a858b31395a21c953
Christian E <christianelena92@gmail.com>
2021-10-16 12:20:10 -0500

    sanitize app, and add Dice.js file

---
+++ src/Dice.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+/** A game can be any number of Dies, all with random values. */
+function Dice(props) {
+  return <section className="Dice"></section>;
+}
+
+export default Dice;
